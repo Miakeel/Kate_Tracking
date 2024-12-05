dda2bb7ac01e425f07c14f1413ff96bd1a13a27c
Mihai <mihaimotoroi@gmail.com>
2024-12-05 19:08:12 +0200

    test

--- Tracking/models.py
+++ Tracking/models.py
@@ -8,7 +8,7 @@ class Participant(models.Model):
     first_name=models.CharField(null=False, max_length=50)
     last_name=models.CharField(null=False, max_length=50)
     institution=models.CharField(null=False, max_length=50)
-    email=models.EmailField(null=False, max_length=50)
+    email=models.EmailField(null=False, max_length=50, unique=True)
     title=models.CharField(null=False, max_length=50)
     phone=models.CharField(blank=True,max_length=14)
     city=models.CharField(null=False, max_length=50)
