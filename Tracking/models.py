a8ac51efdebad3f059ce5485e4b013ba2427b43f
Miakeel <mihaimotoroi@gmail.com>
2025-02-10 21:07:11 +0200

    Update models.py

--- Tracking/models.py
+++ Tracking/models.py
@@ -8,7 +8,7 @@ class Participant(models.Model):
     first_name=models.CharField(null=False, max_length=50)
     last_name=models.CharField(null=False, max_length=50)
     institution=models.CharField(null=False, max_length=300)
-    email=models.EmailField(null=False, max_length=50, unique=True)
+    email=models.EmailField(null=False, max_length=50, unique=False)
     title=models.CharField(null=False, max_length=50)
     phone=models.CharField(blank=True,max_length=14)
     city=models.CharField(null=False, max_length=50)
