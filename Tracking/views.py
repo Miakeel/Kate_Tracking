5101eb681f5f284fbe0c559c7a9bf1068bb897af
Miakeel <mihaimotoroi@gmail.com>
2025-02-10 21:56:27 +0200

    Update views.py
    info badges update

--- Tracking/views.py
+++ Tracking/views.py
@@ -96,8 +96,10 @@ def participant_info_view(request,id):
             sponsors=["AstraZeneca","Novartis"]
             if participant.institution in sponsors:
                 draw.text(((page_width/2 - fontRegular.getlength(participant.institution))/2,750),participant.institution,'black',fontRegular)
-            else:
+            elif fontRegular.getlength(participant.title)<1240 :
                 draw.text(((page_width/2 - fontRegular.getlength(participant.title))/2,750),participant.title,'black',fontRegular)
+            else:
+                draw.text(((page_width/2 - fontRegular.getlength("Attendee"))/2,750),participant.title,'black',fontRegular)
 
 
                 
